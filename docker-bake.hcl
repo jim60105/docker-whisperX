5489ef6707eb00d62d36ece58b4dcf3c06f78ec0
陳鈞 <jim60105@gmail.com>
2023-08-28 21:16:26 +0800

    refactor

--- docker-bake.hcl
+++ docker-bake.hcl
@@ -48,20 +48,26 @@ target "build" {
     ]
   }
 
-  name = "whisperx-${WHISPER_MODEL}-${LANG}"
+  name       = "whisperx-${WHISPER_MODEL}-${LANG}"
   dockerfile = "Dockerfile"
   tags = [
     "ghcr.io/jim60105/whisperx:${WHISPER_MODEL}-${LANG}",
     "quay.io/jim60105/whisperx:${WHISPER_MODEL}-${LANG}"
   ]
-  platforms = ["linux/amd64"]
+  platforms  = ["linux/amd64"]
+  cache-from = ["type=local,mode=max,src=cache"]
+  cache-to   = ["type=local,mode=max,dest=cache"]
 }
 
 target "no_model" {
   dockerfile = "Dockerfile.no_model"
   tags = [
+    "quay.io/jim60105/whisperx:latest",
+    "ghcr.io/jim60105/whisperx:latest",
     "quay.io/jim60105/whisperx:no_model",
     "ghcr.io/jim60105/whisperx:no_model"
   ]
-  platforms = ["linux/amd64"]
+  platforms  = ["linux/amd64"]
+  cache-from = ["type=local,mode=max,src=cache"]
+  cache-to   = ["type=local,mode=max,dest=cache"]
 }
--- load_align_model.py
+++ load_align_model.py
@@ -30,12 +30,13 @@ DEFAULT_ALIGN_MODELS_HF = {
     "tr": "mpoyraz/wav2vec2-xls-r-300m-cv7-turkish",
     "da": "saattrupdan/wav2vec2-xls-r-300m-ftspeech",
     "he": "imvladikon/wav2vec2-xls-r-300m-hebrew",
-    "vi": 'nguyenvulebinh/wav2vec2-base-vi',
+    "vi": "nguyenvulebinh/wav2vec2-base-vi",
     "ko": "kresnik/wav2vec2-large-xlsr-korean",
     "ur": "kingabzpro/wav2vec2-large-xls-r-300m-Urdu",
     "te": "anuragshas/wav2vec2-large-xlsr-53-telugu",
 }
 
+# From https://github.com/m-bain/whisperX/issues/189#issuecomment-1523392800
 if lang in DEFAULT_ALIGN_MODELS_TORCH:
     model_name = DEFAULT_ALIGN_MODELS_TORCH[lang]
     bundle = torchaudio.pipelines.__dict__[model_name]
@@ -47,4 +48,4 @@ elif lang in DEFAULT_ALIGN_MODELS_HF:
     processor = Wav2Vec2Processor.from_pretrained(model_name)
     align_model = Wav2Vec2ForCTC.from_pretrained(model_name)
 else:
-  raise ValueError(f"Unsupported language: {lang}")
+    raise ValueError(f"Unsupported language: {lang}")
